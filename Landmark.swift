aca92e4f0fa3025ab41c0864f583c0b08bdbf0c6
Kathleen Garrity <42784736+kgarrity22@users.noreply.github.com>
2020-04-27 15:39:36 -0400

    Added files via upload

---
+++ Landmark.swift
@@ -0,0 +1,32 @@
+//
+//  Landmark.swift
+//  
+//
+//  Created by Kathleen Garrity on 4/27/20.
+//
+
+import Foundation
+import CoreLocation
+
+class Landmark {
+    var landmarkName: String
+    var landmarkHistory: String
+    var coordinate: Int
+    var createdOn: Date()
+    var postingUserID: String
+    var documentID: String
+    
+    
+}
+
+/*
+ 
+ Create a places class
+ With
+ Landark name
+ History
+ Coordinate
+ postingUserID
+ documentID
+ Created on
+ */
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.0'
+
+target 'TravelWise' do
+
+  use_frameworks!
+
+  pod 'GooglePlaces'
+  pod 'GoogleMaps'
+  pod 'Firebase/Core'
+  pod 'Firebase/Storage'
+  pod 'Firebase/Firestore'
+  pod 'FirebaseUI/Auth'
+  pod 'FirebaseUI/Google'
+  pod 'SDWebImage', '~> 5.0'
+  # add the Firebase pod for Google Analytics
+  pod 'Firebase/Analytics'
+  pod 'Firebase/MLVision'
+
+end
